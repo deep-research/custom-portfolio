9d5e585f6b019e8be167ad55cb9f24c62f627074
Victor <deep-research>
2018-04-30 16:44:36 -0500

    Basic File Structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2 className="jumbotron">Welcome to my website!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
