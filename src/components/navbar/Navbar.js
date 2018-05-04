21d40a7137cdd8c159d4f972c1bcc191b1c793e4
Victor <deep-research>
2018-05-04 16:00:55 -0500

    Page Routing Works

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import "./navbar.css"
+import { Link } from "react-router-dom"
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <nav className="navbar justify-content-around navbar-light bg-light">
-                    <a className="navbar-brand text-danger font-weight-bold">Victor's Coding Revolution!</a>
-                    <form className="form-inline">
-                            <a class="nav-link" href="#">ABOUT</a>
-                            <a class="nav-link" href="#">PROJECTS</a>
-                            <a class="nav-link" href="#">CONTACT</a>
-                    </form>
-                </nav>
-            </div>
+            <nav className="navbar justify-content-around navbar-light bg-light">
+                <Link class="navbar-brand font-weight-bold" to="/projects">Victor's Coding Revolution!</Link>
+                <form className="form-inline">
+                        <Link class="nav-link" to="/about">ABOUT</Link>
+                        <Link class="nav-link" to="/projects">PROJECTS</Link>
+                        <Link class="nav-link" to="/contact">CONTACT</Link>
+                </form>
+            </nav>
         );
     }
 }
