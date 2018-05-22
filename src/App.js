25bc2c72b1ccc97e1300fa01e6652ea087db972f
Victor <deep-research>
2018-05-22 12:25:17 -0500

    Card Created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar"
+import Card from "./components/card/Card"
 import {BrowserRouter as Router, Route, Switch, Redirect}
     from "react-router-dom"
 import About from "./pages/About"
@@ -15,12 +16,21 @@ class App extends Component {
                     <Navbar  />
 
                     <Switch>
-                        <Route exact path="/" render={() => (
-                            <Redirect to="/projects" />
-                        )}/>
-                        <Route path="/about" component={About}/>
-                        <Route path="/projects" component={Projects}/>
-                        <Route path="/contact" component={Contact}/>
+                        <div className="container">
+                            <br />
+                            <div class="row">
+                                <div class="col-md-8 col-sm-7">
+                                    <Route exact path="/" render={() => (
+                                        <Redirect to="/projects" />)}/>
+                                    <Route path="/about" component={About}/>
+                                    <Route path="/projects" component={Projects}/>
+                                    <Route path="/contact" component={Contact}/>
+                                </div>
+                                <div class="col-md-4 col-sm-5">
+                                        <Card />
+                                </div>
+                            </div>
+                        </div>
                     </Switch>
                 </div>
             </Router>
---
+++ src/components/card/Card.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import "./card.css"
+import github from "./images/github.png"
+import linkedIn from "./images/linkedIn.png"
+import stackOverflow from "./images/stackOverflow.png"
+
+class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-header">
+                    Social Media
+                </div>
+                <div className="card-body d-flex flex-row  justify-content-center">
+                    <a href="https://www.github.com/deep-research"
+                        target="_blank" rel="noopener noreferrer"
+                        className="p-2" title="Github">
+                        <img
+                            src={github} alt="GitHub"
+                            className="img-fluid socialMedia"
+                        />
+                    </a>
+                    <a href="https://www.linkedin.com/in/victor-fisher-3220a4143/"
+                        target="_blank" rel="noopener noreferrer"
+                        className="p-2"  title="LinkedIn">
+                        <img
+                            src={linkedIn} alt="LinkedIn"
+                            className="img-fluid socialMedia"
+                        />
+                    </a>
+                    <a href="https://stackoverflow.com/users/8310067/deep-research"
+                        target="_blank" rel="noopener noreferrer"
+                        className="p-2"  title="Stack Overflow">
+                        <img
+                            src={stackOverflow} alt="Stack Overflow"
+                            className="img-fluid socialMedia"
+                        />
+                    </a>
+                </div>
+
+                {/* <div className="card-body d-flex flex-row">
+                    <a className="p-2" href="https://www.github.com/deep-research" target="_blank"><img src="images/github.png" alt="GitHub" className="img-fluid"></a>
+                    <a className="p-2" href="https://www.linkedin.com/in/victor-fisher-3220a4143/" target="_blank"><img src="images/linkedin.png" alt="LinkedIn" className="img-fluid"></a>
+                    <a className="p-2" href="https://stackoverflow.com/users/8310067/deep-research" target="_blank"><img src="images/stack-overflow.png" alt="Stack Overflow" className="img-fluid"></a>      
+                </div> */}
+            </div>
+        );
+    }
+}
+
+export default Card;
