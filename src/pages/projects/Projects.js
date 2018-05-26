7679190e260ae02c7cf1fdcaedbc0f3e3798c19d
Victor <deep-research>
2018-05-26 18:31:15 -0500

    Image Gallery Evened Out

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -5,10 +5,23 @@ import images from "./images/images.json"
 class Projects extends Component {
     imgRow() {
         const galleryCode = [];
+
         for (var i=0; i<images.length; i+=2) {
             galleryCode.push(
-                <div>
-                    <img
+                <div class="row">
+                    <div class="gallery-unit col-md-6 col-sm-12">
+                        <a href={images[i].link} class="thumbnail">
+                            <img class="img-fluid mb-4" src={images[i].image} />
+                            <div class="gallery-label text-center">{images[i].name}</div>
+                        </a>
+                    </div>
+                    <div class="gallery-unit col-md-6 col-sm-12">
+                        <a href={images[i+1].link} class="thumbnail">
+                            <img class="img-fluid mb-4" src={images[i+1].image} />
+                            <div class="gallery-label text-center">{images[i+1].name}</div>
+                        </a>
+                    </div>
+                    {/* <img
                         src={images[i].image}
                         key={images[i].id}
                         id={images[i].id}
@@ -17,8 +30,8 @@ class Projects extends Component {
                         alt={images[i].name}
                         occupation={images[i].occupation}
                         location={images[i].location}>
-                    </img>
-                    <img
+                    </img> */}
+                    {/* <img
                         src={images[i+1].image}
                         key={images[i+1].id}
                         id={images[i+1].id}
@@ -27,7 +40,7 @@ class Projects extends Component {
                         alt={images[i+1].name}
                         occupation={images[i+1].occupation}
                         location={images[i+1].location}>
-                    </img>
+                    </img> */}
                 </div>
             )            
         }
