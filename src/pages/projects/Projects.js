7aad8525c1de1d710e4836af62af9ae3477ce192
Victor <deep-research>
2018-05-26 17:13:33 -0500

    Dynamic Image Gallery

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -3,25 +3,44 @@ import "./projects.css";
 import images from "./images/images.json"
 
 class Projects extends Component {
+    imgRow() {
+        const galleryCode = [];
+        for (var i=0; i<images.length; i+=2) {
+            galleryCode.push(
+                <div>
+                    <img
+                        src={images[i].image}
+                        key={images[i].id}
+                        id={images[i].id}
+                        className="gallery-item m-2"
+                        name={images[i].name}
+                        alt={images[i].name}
+                        occupation={images[i].occupation}
+                        location={images[i].location}>
+                    </img>
+                    <img
+                        src={images[i+1].image}
+                        key={images[i+1].id}
+                        id={images[i+1].id}
+                        className="gallery-item m-2"
+                        name={images[i+1].name}
+                        alt={images[i+1].name}
+                        occupation={images[i+1].occupation}
+                        location={images[i+1].location}>
+                    </img>
+                </div>
+            )            
+        }
+        return galleryCode
+    };
+
     render() {
         return (
             <div>
                 <h2 >My Projects</h2>
                 <hr class="mb-4 hr-thick" />
                 <div id="img-gallery">
-                    {images.map(image => (
-                        /* Data is passed to every image with props */
-                        <img
-                            src={image.image}
-                            key={image.id}
-                            id={image.id}
-                            className="gallery-item"
-                            name={image.name}
-                            alt={image.name}
-                            occupation={image.occupation}
-                            location={image.location}>
-                        </img>
-                    ))}
+                    {this.imgRow()}
                 </div>
             </div>
         );
