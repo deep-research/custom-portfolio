ed11f30b7c5b8ce3d9373fd0e6ccd9638ecab40e
Victor <deep-research>
2018-05-26 21:59:25 -0500

    Gallery Links Working

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import "./projects.css";
-import images from "./images.json"
+import projects from "./projects.json"
 
 class Projects extends Component {
     imgRow() {
         const galleryCode = [];
 
-        for (var i=0; i<images.length; i+=2) {
+        for (var i=0; i<projects.length; i+=2) {
             galleryCode.push(
                 <div className="row" key={i}>
                     <div className="gallery-unit col-md-6 col-sm-12">
-                        <a href={images[i].link} className="thumbnail">
-                            <img className="img-fluid mb-4" src={images[i].image} alt={images[i].name} key={images[i].id} />
-                            <div className="gallery-label text-center">{images[i].name}</div>
+                        <a href={projects[i].link} className="thumbnail" target="_blank">
+                            <img className="img-fluid mb-4" src={projects[i].image} alt={projects[i].name} key={projects[i].id} />
+                            <div className="gallery-label text-center">{projects[i].name}</div>
                         </a>
                     </div>
                     <div className="gallery-unit col-md-6 col-sm-12">
-                        <a href={images[i+1].link} className="thumbnail">
-                            <img className="img-fluid mb-4" src={images[i+1].image} alt={images[i].name} key={images[i].id} />
-                            <div className="gallery-label text-center">{images[i+1].name}</div>
+                        <a href={projects[i+1].link} className="thumbnail" target="_blank">
+                            <img className="img-fluid mb-4" src={projects[i+1].image} alt={projects[i].name} key={projects[i].id} />
+                            <div className="gallery-label text-center">{projects[i+1].name}</div>
                         </a>
                     </div>
                 </div>
